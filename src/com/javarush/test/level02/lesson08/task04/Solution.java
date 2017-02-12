f2e5cc5629d249e2ed1f83621881d796457c6c82
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2017-02-12 23:49:21 +0200

    Added a new tasks to level 02 (lessons 08).

---
+++ src/com/javarush/test/level02/lesson08/task04/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level02.lesson08.task04;
+
+/* Минимум четырех чисел
+Написать функцию, которая вычисляет минимум из четырёх чисел.
+Использовать функцию из задачи 1.
+*/
+public class Solution
+{
+    public static int min(int a, int b, int c, int d)
+    {
+        return min(min(a, b), min(c, d));
+    }
+
+    public static int min(int a, int b)
+    {
+        int m1;
+        if (a < b)
+            m1 = a;
+        else
+            m1 = b;
+        return m1;
+
+    }
+}
