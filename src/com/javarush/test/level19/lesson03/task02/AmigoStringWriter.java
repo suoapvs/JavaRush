7d76731513aee00ba101cdebbe5bea0652152a67
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-03-24 14:37:25 +0200

    Added a new tasks to level 19 (lesson 03 - task 02).

---
+++ src/com/javarush/test/level19/lesson03/task02/AmigoStringWriter.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level19.lesson03.task02;
+
+import java.io.IOException;
+
+public interface AmigoStringWriter {
+
+    void flush() throws IOException;
+
+    void writeString(String s) throws IOException;
+
+    void close() throws IOException;
+}
