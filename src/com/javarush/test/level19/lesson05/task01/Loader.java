09a66ca3d480c2873403e6486298433bf53408b8
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-05-19 00:36:13 +0300

    Code refactoring.

--- src/com/javarush/test/level19/lesson05/task01/Loader.java
+++ src/com/javarush/test/level19/lesson05/task01/Loader.java
@@ -12,40 +12,48 @@ public class Loader
     private final String path;
     private byte[] bytes;
 
-    public Loader(final String path) {
+    public Loader(final String path)
+    {
         this(path, new byte[0]);
     }
 
-    public Loader(final String path, final byte[] bytes) {
+    public Loader(final String path, final byte[] bytes)
+    {
         this.path = path;
         this.bytes = bytes;
     }
 
     public byte[] read() throws IOException
     {
-        try (final RandomAccessFile in = new RandomAccessFile(this.path, "r")) {
+        try (final RandomAccessFile in = new RandomAccessFile(this.path, "r"))
+        {
             byte[] buffer = new byte[(int) in.length()];
             in.read(buffer);
             return buffer;
         }
     }
 
-    public void write() throws IOException {
-        try (final RandomAccessFile out = new RandomAccessFile(this.path, "rw")) {
+    public void write() throws IOException
+    {
+        try (final RandomAccessFile out = new RandomAccessFile(this.path, "rw"))
+        {
             out.setLength(0);
             out.write(bytes);
         }
     }
 
-    public String getPath() {
+    public String getPath()
+    {
         return this.path;
     }
 
-    public byte[] getBytes() {
+    public byte[] getBytes()
+    {
         return this.bytes;
     }
 
-    public void setBytes(final byte[] bytes) {
+    public void setBytes(final byte[] bytes)
+    {
         this.bytes = bytes;
     }
 }
